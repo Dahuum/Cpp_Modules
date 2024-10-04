c32f9fee7fef77a256f490153e52806278adce8d
48k483x <romaissaeabde@gmail.com>
2024-10-04 15:41:09 +0100

    IThink ex01 wrvw

--- cpp05/ex01/Form.cpp
+++ cpp05/ex01/Form.cpp
@@ -15,14 +15,14 @@ const char* Form::GradeTooLowException::what() const throw()
     return "Grade Too Low";
 }
 
-Form::Form(const std::string N, int gradeToSign, int gradeToExecute): name(N), GRADE_REQUIRED_TO_SIGN(gradeToSign), GRADE_REQUIRED_TO_EXECUTE(gradeToExecute)
+Form::Form(const std::string N, int gradeToSign, int gradeToExecute): name(N), isSigned(false), GRADE_REQUIRED_TO_SIGN(gradeToSign), GRADE_REQUIRED_TO_EXECUTE(gradeToExecute)
 {
     std::cout << "Form: Parameterized Constructor Called" << std::endl;
     if (gradeToSign <= 0 || gradeToExecute <= 0) throw GradeTooHighException();
     else if (gradeToSign > 150 || gradeToExecute > 150) throw GradeTooLowException();
 }
 
-Form::Form(const Form& other): name(other.name), GRADE_REQUIRED_TO_SIGN(other.GRADE_REQUIRED_TO_SIGN), GRADE_REQUIRED_TO_EXECUTE(other.GRADE_REQUIRED_TO_EXECUTE)
+Form::Form(const Form& other): name(other.name), isSigned(other.isSigned), GRADE_REQUIRED_TO_SIGN(other.GRADE_REQUIRED_TO_SIGN), GRADE_REQUIRED_TO_EXECUTE(other.GRADE_REQUIRED_TO_EXECUTE)
 {
     std::cout << "Form: Copy Constructor Called" << std::endl;
 }
@@ -64,7 +64,7 @@ int Form::getGradeToExecute() const
 
 void Form::beSigned(const Bureaucrat& other)
 {
-    if (other.getGrade() <= this->getGradeToSign()) this->isSigned = true;
+    if (other.getGrade() <= this->getGradeToSign() && this->isSigned == false) this->isSigned = true;
     else throw Form::GradeTooLowException();
 }
 
